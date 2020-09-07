f1bcccad44b64b69eeb630b9239339c503f6b18e
Giovanni Palleschi <gpalleschi@gmail.com>
2020-09-07 16:51:26 +0200

    version 1.1 add Hex Tag e Tap

--- PyAsn1Tk.py
+++ PyAsn1Tk.py
@@ -6,6 +6,8 @@ import os, sys
 #
 # Utilty to read BER Asn1 File in Python3 with GUI Tk
 #
+# v1.0 - First Version
+# v1.1 - 07/09/2020 - Add TAP Notation and Tag Hex Value 
 
 class Application(object):
 
@@ -76,7 +78,21 @@ class Application(object):
 				nextbis=nextbis & 0xff
 				tag = (tag << 7) | ( nextbis & 127)
 
-		CodeTag =  "%s-%s [%s] offset [%s]" % (id,tag,taghex,startByte)
+# Tag rappresentation
+
+		if self.bTypeTAP.get() == False:
+			CodeTag =  "%s-%s" % (id,tag)
+		else:
+			CodeTag =  "%s" % (tag)
+
+# Hex Tag
+		if self.bHexRapr.get() == True:
+			CodeTag = CodeTag +  " [%s] " % (taghex)
+
+# OffSet
+
+		CodeTag = CodeTag + " offset [%s]" % (startByte)
+
 		appo=self.readAsn1(filea)
 		if appo == '':
 			return 1
@@ -147,8 +163,8 @@ class Application(object):
 		self.bTypeTAP.set(False)
 		self.bHexRapr.set(False)
 
-		self.typeTAP = Checkbutton(self.content, text="TAP Notation", variable=self.bTypeTAP, onvalue=False)
-		self.hexRapr = Checkbutton(self.content, text="Tag Hex Value", variable=self.bHexRapr, onvalue=False)
+		self.typeTAP = Checkbutton(self.content, text="TAP Notation", variable=self.bTypeTAP)
+		self.hexRapr = Checkbutton(self.content, text="Tag Hex Value", variable=self.bHexRapr)
 
 		self.select = Button(self.content, text="Select a File", command=self.ReadButton_Click)
 		self.save = Button(self.content, text="Save on File", command=self.SaveButton_Click)
@@ -169,8 +185,8 @@ class Application(object):
 		self.save.grid(column=0,row=2, sticky=(N, E, W), padx=5, pady=5)
 		self.cancel.grid(column=0,row=3, sticky=(N, E, W), padx=5, pady=5)
 # Type TAP and Hex Rapr to develop		
-#		self.typeTAP.grid(column=0,row=4, sticky=(N, E, W), padx=5, pady=5)
-#		self.hexRapr.grid(column=0,row=5, sticky=(N, E, W), padx=5, pady=5)
+		self.typeTAP.grid(column=0,row=4, sticky=(N, E, W), padx=5, pady=5)
+		self.hexRapr.grid(column=0,row=5, sticky=(N, E, W), padx=5, pady=5)
 		self.bQuit.grid(column=0,row=6, sticky=(S, E, W), padx=5, pady=5)
 
 		parent.columnconfigure(0, weight=1)
@@ -189,45 +205,7 @@ class Application(object):
 		self.content.rowconfigure(5, weight=1)
 		self.content.rowconfigure(6, weight=1)
 
-		self.parent.title('PyAsn1Tk 1.0')
-
-# Tk Interface Definicion		
-#	def __init__(self, parent):
-#		self.parent= parent
-#
-#		self.textFrame=Frame(self.parent)
-#		
-#		self.lyricsText = Text(self.textFrame,width=130,height=30)
-#		self.lyricsText.pack({'side':'left'})
-##		self.lyricsText.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-#
-##		self.lyricsScrollbar=Scrollbar(self.textFrame)
-#		self.lyricsScrollbar.pack({'side':'left', 'expand':'yes', 'fill':'y'})
-#
-#		self.textFrame.pack()
-#
-#		self.lyricsScrollbar['command']=self.lyricsText.yview
-#		self.lyricsText['yscrollcommand']=self.lyricsScrollbar.set
-#
-#		self.ReadButton=Button(self.parent,text="Select a File",command=self.ReadButton_Click)
-#		self.ReadButton.pack()
-#
-##		self.DeleteButton=Button(self.parent,text="Save on File",command=self.SaveButton_Click)
-#		self.DeleteButton.pack()
-#
-#		self.DeleteButton=Button(self.parent,text="Clear All",command=self.ClearButton_Click)
-#		self.DeleteButton.pack()
-#
-#		self.OutputLabel = Label(self.parent, text="")
-#		self.OutputLabel.pack()
-#
-##        self.imq=PhotoImage(file='exit.gif')
-#        self.imq=Button('exit');
-    
-#		self.QuitButton=Button(self.parent,text="Quit",command=self.Quit)
-#		self.QuitButton.pack()
-
-#		self.parent.title('PyAsn1Tk 1.0')
+		self.parent.title('PyAsn1Tk 1.1')
 
 	def ReadButton_Click(self):
 		filename = filedialog.askopenfilename()
@@ -251,8 +229,12 @@ class Application(object):
 			fileasn1s=open(filename,"w")
 			fileasn1s.write(self.txtTrad.get(1.0,END))
 			fileasn1s.close()
+
 	def ClearButton_Click(self):
 		self.txtTrad.delete(1.0, END)
+		self.bTypeTAP.set(False)
+		self.bHexRapr.set(False)
+
 	def Quit(self):
 		self.parent.destroy()
 
